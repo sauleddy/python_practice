a07bfd9570fa2a1122e7b72559940f9fac14a2e5
Eddy Hsu <sauleddy38@gmail.com>
2017-06-17 21:50:15 +0800

    1. modify encode sample

--- encode/main.py
+++ encode/main.py
@@ -1,9 +1,20 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+print('char to integer')
+print(ord('a'))
+print('integer to char')
+print(chr(97))
+
+print('string to bytes')
 my_str = '你好'
 # print(my_str.encode('ascii'))
 my_str_utf8 = my_str.encode('utf-8')
 print(my_str_utf8)
 my_str_unicode = my_str_utf8.decode('utf-8')
 print(my_str_unicode)
+
+print('format')
+print('Hello %s %d %x %f' % ('World', 10, 10, 10.5))
+
+
