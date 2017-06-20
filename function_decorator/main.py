10c0311585e6daec87e89960484c0141c6a3c5cd
Eddy Hsu <sauleddy38@gmail.com>
2017-06-20 14:51:05 +0800

    1. finetune closure and decorator samples

--- function_decorator/main.py
+++ function_decorator/main.py
@@ -3,18 +3,19 @@ import datetime
 import functools
 
 
-def add_time(func):
-    @functools.wraps(func)
-    def print_time_log(log):
-        log = str(datetime.datetime.now()) + " : " + log
-        func(log)
-    return print_time_log
+def print_log_ex_ex(text):
+    def print_log_ex(func):
+        def print_all_log(*arg):
+            print('begin call %s %s' % (text, func.__name__))
+            func(arg[0])
+            print('end call')
+        return print_all_log
+    return print_log_ex
 
 
-@add_time
-def print_log(log):
-    print(log)
+@print_log_ex_ex('execute')
+def print_log(*arg):
+    print(arg[0])
 
 
-print_log('This is my log.')
-print(print_log.__name__)
+print_log('Today is not my day.')
